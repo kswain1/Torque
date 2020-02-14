998497334f970adc0f5a095c5d806aed56dcaf16
Kehlin <kehlinswain@yahoo.com>
2020-02-14 12:42:38 -0600

    model files update

---
+++ TestIos/VIews/OrgViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OrgViewController.swift
+//  TestIos
+//
+//  Created by kehlin swain on 2/12/20.
+//  Copyright © 2020 kehlin swain. All rights reserved.
+//
+
+import UIKit
+
+class OrgViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
